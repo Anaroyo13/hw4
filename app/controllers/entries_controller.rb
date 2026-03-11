ab2e35d19fbfdba9d31445e01bda1b5dbb495ab8
Anaroyo13 <ana.royo@kellogg.northwestern.edu>
2026-03-11 21:59:49 +0000

    Attach image after entry save

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,24 +4,26 @@ class EntriesController < ApplicationController
   def new
   end
 
-  def create
-    if @current_user == nil
-      redirect_to "/login"
-      return
-    end
-
-    @entry = Entry.new
-    @entry.place_id = params["place_id"]
-    @entry.title = params["title"]
-    @entry.description = params["description"]
-    @entry.occurred_on = params["occurred_on"]
-    @entry.user_id = @current_user.id
-    @entry.image.attach(params["image"]) if params["image"].present?
-
-    if @entry.save
-      redirect_to "/places/#{@entry.place_id}"
-    else
-      redirect_to "/places"
-    end
+ def create
+  if @current_user == nil
+    redirect_to "/login"
+    return
   end
+
+  @entry = Entry.new
+  @entry.place_id = params["place_id"]
+  @entry.title = params["title"]
+  @entry.description = params["description"]
+  @entry.occurred_on = params["occurred_on"]
+  @entry.user_id = @current_user.id
+
+  if @entry.save
+    if params["image"].present?
+      @entry.image.attach(params["image"])
+    end
+    redirect_to "/places/#{@entry.place_id}"
+  else
+    redirect_to "/places"
+  end
+end
 end
