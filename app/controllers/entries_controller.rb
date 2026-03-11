f196faf931366c98d6751b7a8b603a175249f22c
Anaroyo13 <ana.royo@kellogg.northwestern.edu>
2026-03-11 21:55:07 +0000

    Bypass CSRF verification for Render form posts

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,26 +1,27 @@
 class EntriesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   def new
   end
 
   def create
-  if @current_user == nil
-    redirect_to "/login"
-    return
-  end
+    if @current_user == nil
+      redirect_to "/login"
+      return
+    end
 
-  @entry = Entry.new
-  @entry.place_id = params["place_id"]
-  @entry.title = params["title"]
-  @entry.description = params["description"]
-  @entry.occurred_on = params["occurred_on"]
-  @entry.user_id = @current_user.id
-  @entry.image.attach(params["image"])
+    @entry = Entry.new
+    @entry.place_id = params["place_id"]
+    @entry.title = params["title"]
+    @entry.description = params["description"]
+    @entry.occurred_on = params["occurred_on"]
+    @entry.user_id = @current_user.id
+    @entry.image.attach(params["image"]) if params["image"].present?
 
-  if @entry.save
-    redirect_to "/places/#{@entry.place_id}"
-  else
-    redirect_to "/places"
+    if @entry.save
+      redirect_to "/places/#{@entry.place_id}"
+    else
+      redirect_to "/places"
     end
   end
 end
