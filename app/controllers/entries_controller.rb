d8b79fc65179919355821450bf6cbe83277ac701
Anaroyo13 <ana.royo@kellogg.northwestern.edu>
2026-03-11 20:57:41 +0000

    Adjusting frontend

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -13,8 +13,9 @@ class EntriesController < ApplicationController
   @entry.place_id = params["place_id"]
   @entry.title = params["title"]
   @entry.description = params["description"]
-  @entry.posted_on = params["posted_on"]
+  @entry.occurred_on = params["occurred_on"]
   @entry.user_id = @current_user.id
+  @entry.image.attach(params["image"])
 
   if @entry.save
     redirect_to "/places/#{@entry.place_id}"
