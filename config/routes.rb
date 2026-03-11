fb7d33969dadac99d0e1658f03663d92f1f307e3
Anaroyo13 <ana.royo@kellogg.northwestern.edu>
2026-03-11 20:24:31 +0000

    Finalizing authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
+
+  get("/signup", { :controller => "users", :action => "new" })
+  post("/users", { :controller => "users", :action => "create" })
+
+  get("/login", { :controller => "sessions", :action => "new" })
+  post("/sessions", { :controller => "sessions", :action => "create" })
+  get("/logout", { :controller => "sessions", :action => "destroy" })
+
   resources "entries"
   resources "places"
-  resources "sessions"
-  resources "users"
 end
